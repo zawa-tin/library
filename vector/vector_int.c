eb73c4351ecd2d0cafe78557cc244a2566b8c056
zawakasutera <zawazawanomorichan@outlook.jp>
2022-01-17 22:21:19 +0900

    vectorにerase, pushback, popbackを実装

--- vector/vector_int.c
+++ vector/vector_int.c
@@ -1,45 +1,58 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <limits.h>
 
 typedef struct {
     int * vec;
     int size;
-    int max_size;
-    int empty;
-    int begin;
-    int end;
 }vector;
 
 void construct(vector* const this, int n) {
     this->vec = (int *)malloc(sizeof(int) * n);
     this->size = n;
-    this->empty = 1;
-    this->begin = INT_MAX;
-    this->end = -1;
+}
+
+void resize(vector* const this, int new_size) {
+    int *new = (int *)realloc(this->vec, sizeof(int) * new_size);
+    this->vec = new;
+    this->size = new_size;
 }
 
 void insert(vector* const this, int value, int place) {
     if (place >= this->size)
-        exit(8); //適当
+        resize(this, place + 1);
     this->vec[place] = value;
-
-    if (place < this->begin)
-        this->begin = place;
-
-    if (place > this->end)
-        this->end = place;
 }
 
-void resize(vector* const this, int new_size) {
-    (int *)realloc(this, sizeof(new_size));
-    this->size = new_size;
+int erase(vector* const this, int place) {
+    if (place >= this->size)
+        return 0;
+    
+    int erased_value = this->vec[place];
+    
+    for (int i = place ; i < this->size - 1 ; i++)
+        this->vec[i] = this->vec[i + 1];
+    
+    resize(this, this->size - 1);
+
+    return erased_value;
 }
+
+void pushback(vector* const this, int value) {
+    resize(this, this->size + 1);
+
+    insert(this, value, this->size - 1);
+}
+
+int popback(vector* const this) {
+    int poped_value;
+    if (this->size > 0)
+        poped_value = erase(this, this->size - 1);
+    else
+        return -1;
+        
+}
+
 
 int main() {
 
-    vector sample;
-    construct(&sample, 5);
-
-    insert(&sample, 3, 1);
 }
