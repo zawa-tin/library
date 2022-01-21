abb8128aa39215053386f645aee9429fbf231a49
zawakasutera <zawazawanomorichan@outlook.jp>
2022-01-21 15:26:52 +0900

    vol1

--- queue/queue_int.c
+++ queue/queue_int.c
@@ -15,10 +15,81 @@ queue queue_constructor(queue self, int size) {
     self->head = 0;
     self->tail = 0;
     self->array = (int *)malloc(sizeof(int) * (size + 1));
+
+    return self;
 }
 
 queue queue_destructor(queue self) {
     self->head = self->tail = self->size = 0;
     free(self->array);
     free(self);
+}
+
+int is_queue_empty(queue self) {
+    if (self->head == self->tail)
+        return 1;
+    else
+        return 0;
+}
+
+int is_queue_full(queue self) {
+    if ((self->head == self->tail + 1) || (self->head == 0 && self->tail == self->size))
+        return 1;
+    else
+        return 0;
+}
+
+void queue_enqueue(queue self, int value) {
+    if (!is_queue_full(self)) {
+        self->array[self->tail] = value;
+        if (self->tail == self->size)
+            self->tail = 0;
+        else
+            self->tail++;
+    }
+    else
+        // queue overflow
+        ;
+}
+
+int queue_dequeue(queue self) {
+    if (!is_queue_empty(self)) {
+        int dequeued_value = self->array[self->head];
+        if (self->head == self->size)
+            self->head = 0;
+        else
+            self->head++;
+        
+        return dequeued_value;
+    }
+    else
+        // queue underflow
+        return -1;
+}
+
+// for debug
+void queue_print(queue self) {
+    printf("size = %d\n", self->size);
+    for (int i = 0 ; i < self->size + 1 ; i++)
+        printf(" %d", self->array[i]);
+    printf("\n");
+    printf("head = %d(value is %d), tail = %d\n", self->head, self->array[self->head], self->tail);
+}
+
+int main() {
+    queue sample;
+    sample = queue_constructor(sample, 6);
+    
+    queue_enqueue(sample, 3);
+    queue_enqueue(sample, 88);
+    queue_enqueue(sample, 14);
+    queue_print(sample);
+    int a = queue_dequeue(sample);
+    printf("%d is dequeued\n", a);
+    queue_print(sample);
+    a = queue_dequeue(sample);
+    printf("%d is dequeued\n", a);
+    queue_print(sample);
+    queue_enqueue(sample, 109);
+    queue_print(sample);
 }
