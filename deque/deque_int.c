fc2f2b55cb45c981a677c93685c8f5a155ff2424
zawakasutera <zawazawanomorichan@outlook.jp>
2022-01-21 22:44:27 +0900

    pushとpop実装 明日デバッグ

--- deque/deque_int.c
+++ deque/deque_int.c
@@ -24,3 +24,79 @@ deque deque_destructor(deque self) {
     free(self->array);
     free(self);
 }
+
+int is_deque_empty(deque self) {
+    if (self->front + 1 == self->back || (self->back == 0 && self->front == self->size))
+        return 1;
+    else
+        return 0;
+}
+
+int is_deque_full(deque self) {
+    if (self->front == self->back)
+        return 1;
+    else
+        return 0;
+}
+
+void deque_push_front(deque self, int value) {
+    if (!is_deque_full(self)) {
+        self->array[self->front] = value;
+        if (self->front == 0)
+            self->front = self->size;
+        else
+            self->front--;
+    }
+    else
+        // deque overflow
+        ;
+}
+
+void deque_push_back(deque self, int value) {
+    if (!is_deque_full(self)) {
+        self->array[self->back] = value;
+        if (self->back == self->size)
+            self->back = 0;
+        else
+            self->back++;
+    }
+    else
+        // deque overflow
+        ;
+}
+
+int deque_pop_front(deque self) {
+    if (!is_deque_empty(self)) {
+        if (self->front == self->size)
+            self->front = 0;
+        else
+            self->front++;
+        
+        return self->array[self->front];
+    }
+    else
+        // deque underflow
+        ;
+}
+
+int deque_pop_back(deque self) {
+    if (!is_deque_empty(self)) {
+        if (self->back == 0)
+            self->back = self->size;
+        else
+            self->back--;
+        
+        return self->array[self->back];
+    }
+    else
+        // deque underflow
+        ;
+}
+
+void deque_print(deque self) {
+    printf("size is %d\n", self->size);
+    for (int i = 0 ; i <= self->size ; i++)
+        printf(" %d", self->array[i]);
+    printf("\n");
+    printf("front is %d,  back is %d\n", self->front, self->back);
+}
