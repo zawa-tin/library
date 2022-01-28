f29271bf32d5796625d660a0efe09c8a22952683
zawakasutera <zawazawanomorichan@outlook.jp>
2022-01-28 17:42:47 +0900

    clang fl

--- queue/queue_int.c
+++ queue/queue_int.c
@@ -1,15 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
-struct queue_entity {
+struct queue_entity
+{
     int size;
     int head;
     int tail;
     int *array;
 };
-typedef struct queue_entity * queue;
+typedef struct queue_entity *queue;
 
-queue queue_constructor(queue self, int size) {
+queue queue_constructor(queue self, int size)
+{
     self = (queue)malloc(sizeof(struct queue_entity));
     self->size = size;
     self->head = 0;
@@ -19,28 +22,33 @@ queue queue_constructor(queue self, int size) {
     return self;
 }
 
-queue queue_destructor(queue self) {
+queue queue_destructor(queue self)
+{
     self->head = self->tail = self->size = 0;
     free(self->array);
     free(self);
 }
 
-int is_queue_empty(queue self) {
+bool is_queue_empty(queue self)
+{
     if (self->head == self->tail)
-        return 1;
+        return true;
     else
-        return 0;
+        return false;
 }
 
-int is_queue_full(queue self) {
+bool is_queue_full(queue self)
+{
     if ((self->head == self->tail + 1) || (self->head == 0 && self->tail == self->size))
-        return 1;
+        return true;
     else
-        return 0;
+        return false;
 }
 
-void queue_enqueue(queue self, int value) {
-    if (!is_queue_full(self)) {
+void queue_enqueue(queue self, int value)
+{
+    if (!is_queue_full(self))
+    {
         self->array[self->tail] = value;
         if (self->tail == self->size)
             self->tail = 0;
@@ -52,14 +60,16 @@ void queue_enqueue(queue self, int value) {
         ;
 }
 
-int queue_dequeue(queue self) {
-    if (!is_queue_empty(self)) {
+int queue_dequeue(queue self)
+{
+    if (!is_queue_empty(self))
+    {
         int dequeued_value = self->array[self->head];
         if (self->head == self->size)
             self->head = 0;
         else
             self->head++;
-        
+
         return dequeued_value;
     }
     else
@@ -67,7 +77,8 @@ int queue_dequeue(queue self) {
         ;
 }
 
-int queue_front(queue self) {
+int queue_front(queue self)
+{
     if (!is_queue_empty(self))
         return self->array[self->head];
     else
@@ -76,9 +87,10 @@ int queue_front(queue self) {
 }
 
 // for debug
-void queue_print(queue self) {
+void queue_print(queue self)
+{
     printf("size = %d\n", self->size);
-    for (int i = 0 ; i < self->size + 1 ; i++)
+    for (int i = 0; i < self->size + 1; i++)
         printf(" %d", self->array[i]);
     printf("\n");
     printf("head = %d(value is %d), tail = %d\n", self->head, self->array[self->head], self->tail);
