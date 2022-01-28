5f841951c1cfc7a980adeb8652a42e69a255bab5
zawakasutera <zawazawanomorichan@outlook.jp>
2022-01-28 17:30:51 +0900

    微調整、bool値導入

--- stack/stack_int.c
+++ stack/stack_int.c
@@ -1,14 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
-struct stack_entity {
+struct stack_entity
+{
     int size;
     int top;
     int *array;
 };
 typedef struct stack_entity *stack;
 
-stack stack_constructor(stack self, int size) {
+stack stack_constructor(stack self, int size)
+{
     self = (stack)malloc(sizeof(struct stack_entity));
     self->size = size;
     self->top = 0;
@@ -16,58 +19,68 @@ stack stack_constructor(stack self, int size) {
     return self;
 }
 
-void stack_destructor(stack self) {
+void stack_destructor(stack self)
+{
     self->size = 0;
     self->top = 0;
     free(self->array);
     free(self);
 }
 
-int is_stack_empty(stack self) {
+bool is_stack_empty(stack self)
+{
     if (self->top == 0)
-        return 1;
+        return true;
     else
-        return 0;
+        return false;
 }
 
-int is_stack_full(stack self) {
+bool is_stack_full(stack self)
+{
     if (self->top == self->size)
-        return 1;
+        return true;
     else
-        return 0;
+        return false;
 }
 
-void stack_push(stack self, int value) {
-    if (!is_stack_full(self)) {
+void stack_push(stack self, int value)
+{
+    if (!is_stack_full(self))
+    {
         self->top++;
         self->array[self->top] = value;
     }
     else
-        // overflow!
+        // stack overflow
         ;
 }
 
-int stack_pop(stack self) {
-    if (!is_stack_empty(self)) {
+int stack_pop(stack self)
+{
+    if (!is_stack_empty(self))
+    {
         self->top--;
         return self->array[self->top + 1];
     }
     else
-        // underflow
+        // stack underflow
         ;
 }
 
-int stack_top(stack self) {
+int stack_top(stack self)
+{
     if (!is_stack_empty(self))
         return self->array[self->top];
     else
-        // underflow
+        // stack underflow
         ;
 }
 
 // for debug
-void stack_print(stack self) {
-    for (int i = 0 ; i <= self->size ; i++) {
+void stack_print(stack self)
+{
+    for (int i = 0; i < self->size + 1; i++)
+    {
         printf(" %d", self->array[i]);
     }
     printf("\n");
